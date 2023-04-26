d6f8f0c1874713540c413ad7126e0cc7b663b6e5
Nandini Deodia <nandinideodia.2406@gmail.com>
2023-04-26 09:15:20 +0530

    second

---
+++ scarping.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Specify the URL of the website to scrape
+url = 'https://www.glassdoor.co.in/Reviews/Wipro-Reviews-E9936.htm'
+
+# Send a GET request to the URL
+response = requests.get(https://www.glassdoor.co.in/Reviews/Wipro-Reviews-E9936.htm)
+
+# Parse the HTML content of the response using BeautifulSoup
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Find elements on the page using their HTML tags and attributes
+title = soup.find('title').text
+links = [a['href'] for a in soup.find_all('a')]
+
+# Print the scraped data
+print('Title:', title)
+print('Links:', links)
